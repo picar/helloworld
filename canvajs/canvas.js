4f4fd489000409428349b8858b0523eb9837adfa
picar <pierre.carette@gmail.com>
2012-11-18 21:32:31 -0800

    Started monster memo game

--- canvajs/canvas.js
+++ canvajs/canvas.js
@@ -1,24 +1,44 @@
+var monsterImage;
+
+function resize_canvas(){
+	canvas = document.getElementById("canvas");
+    if (canvas.width  < window.innerWidth - 25) {
+    	canvas.width  = window.innerWidth - 25;
+    }
+	if (canvas.height < window.innerHeight - 25) {
+    	canvas.height = window.innerHeight - 25;
+	}
+}
+
+function draw_card(canvasContext, card_number, x, y) {
+	canvasContext.drawImage(monsterImage, 25, 25, 225, 350, 5 + 155 * x, 5 + 230 * y, 150, 225);
+}
+
 function init(){
-var ctx = document.getElementById('canvas').getContext('2d');
+var canvasElement = $("#canvas")[0];
+var ctx = canvasElement.getContext('2d');
+monsterImage = new Image();
+monsterImage.src = "monsters.jpg";
+resize_canvas();
 
-ctx.fillStyle = "#00A308";
+ctx.fillStyle = "#099909";
 ctx.beginPath();
-ctx.arc(220, 220, 50, 0, Math.PI * 2, true);
+ctx.rect(0, 0, canvasElement.width, canvasElement.height);
 ctx.closePath();
 ctx.fill();
 
-ctx.fillStyle = "#FF1C0A";
-ctx.beginPath();
-ctx.arc(100, 100, 100, 0, Math.PI * 2, true);
-ctx.closePath();
-ctx.fill();
+draw_card(ctx, 0, 0, 0);
+draw_card(ctx, 0, 1, 0);
+draw_card(ctx, 0, 2, 0);
+draw_card(ctx, 0, 3, 0);
+draw_card(ctx, 0, 4, 0);
+draw_card(ctx, 0, 5, 0);
+draw_card(ctx, 0, 6, 0);
+draw_card(ctx, 0, 7, 0);
+draw_card(ctx, 0, 0, 1);
+draw_card(ctx, 0, 0, 2);
+draw_card(ctx, 0, 0, 3);
 
-//the rectangle is half transparent
-ctx.fillStyle = "rgba(255, 255, 0, .5)"
-ctx.beginPath();
-ctx.rect(15, 150, 120, 120);
-ctx.closePath();
-ctx.fill();
 }
 
 window.addEventListener("load", init, false);
